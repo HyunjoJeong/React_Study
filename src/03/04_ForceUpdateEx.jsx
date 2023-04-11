e785d9cecd1a56baea18f967b03dc142fb5c245e
HyunjoJeong <twinkleandy@snu.ac.kr>
2023-04-12 07:12:52 +0900

    Ch.3 done

--- src/03/04_ForceUpdateEx.jsx
+++ src/03/04_ForceUpdateEx.jsx
@@ -4,7 +4,8 @@ class ForceUpdateEx extends Component {
   constructor(props) {
     super(props);
 
-    // 아래와 같이 state 객체 변수를 만들지 않고 그냥 써도 state가 됨.
+    // 아래와 같이 state 객체 변수를 만들지 않고 그냥 constructor에서 초기화하는 this.변수는 state로 쓸 수 있음(?)
+    // property와 차이점? : property는 속성의 형태로 위에서 아래로 전달..
     this.loading = true;
     this.formData = 'no data';
 
